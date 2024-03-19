d3d95966dee63ca955993608d95dd244bc7e2343
deya19 <deyasaleh2@gmail.com>
2024-03-19 12:37:08 +0300

    3D Portfolio

---
+++ src/models/Sky.jsx
@@ -0,0 +1,22 @@
+import { useGLTF } from "@react-three/drei";
+
+import skyScene from "../assets/3d/sky.glb";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
+export default function Sky({ isRotating }) {
+  const { scene } = useGLTF(skyScene);
+  const skyRef = useRef();
+
+  useFrame((_, delta) => {
+    if(isRotating){
+      skyRef.current.rotation.y += 0.15 * delta;
+    }
+  }); // it is responsible of move the sky
+
+  return (
+    <mesh ref={skyRef} >
+      <primitive object={scene} />
+    </mesh>
+  );
+}
